533148abe28ed508fae46828407e3642733e0ef6
sayyadsajiya <71958142+sayyadsajiya@users.noreply.github.com>
2020-10-01 05:53:13 -0700

    Add files via upload

---
+++ Surface.java
@@ -0,0 +1,35 @@
+package BuildinSunlight;
+
+public class Surface {
+	
+    static int countBuildings(int arr[], int n) 
+    { 
+        
+        int count = 1; 
+       
+         
+        int curr_max = arr[0]; 
+        for (int i=1; i<n; i++) 
+        { 
+           
+            if (arr[i] > curr_max) 
+            { 
+                count++; 
+                curr_max=arr[i]; 
+            } 
+        } 
+       
+        return count; 
+    } 
+      
+    
+    public static void main(String[] args)  
+    { 
+        int arr[] = {7, 4, 8, 2, 9}; 
+          
+        System.out.println(countBuildings(arr, arr.length)); 
+          
+    } 
+
+
+}
